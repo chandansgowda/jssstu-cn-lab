c083aef56ca37cdafe12f98b52b673d0ff95e73f
Chandan Gowda <41890434+chandansgowda@users.noreply.github.com>
2023-02-06 12:41:15 +0530

    Update leaky_bucket.cpp

--- leaky_bucket.cpp
+++ leaky_bucket.cpp
@@ -4,12 +4,16 @@ using namespace std;
 int main(){
     int no_of_queries = 4;
     int bucket_size = 10;
-    int input_packet_size = 4;
+    int input_packet_size;
     int output_packet_size = 1;
     int stored_buffer_size = 0;
     int size_left;
 
     for(int i=0; i<no_of_queries; i++){
+        
+        cout<<"Input Packet Size: ";
+        cin>>input_packet_size;
+        
         size_left = bucket_size - stored_buffer_size;
         if (input_packet_size<=size_left){
             stored_buffer_size+=input_packet_size;
